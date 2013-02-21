a7fd72395a4dd2b6bd1d278f6a8cb63e425fc530
Elisabeth Mansel <yuki@juokasia-pro.fritz.box>
2013-02-21 23:17:18 +0100

    init

---
+++ app/controllers/example_controller.rb
@@ -0,0 +1,5 @@
+class ExampleController < ApplicationController
+  def test
+  @current_time = Time.now
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTestproject::Application.config.secret_token = '8513fb296807f3624e16aa77185ee4e64a23422736aa72683d5e4cb5858431188a800112607c65d0e655ec60f4ae099d0d6b6349889f7e37545f34c65bc13204'
