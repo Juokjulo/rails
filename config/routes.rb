4b1d091d7d7d2b0c577145265b488bdffba4e4e6
Elisabeth Mansel <yuki@juokasia-pro.fritz.box>
2013-02-22 00:01:56 +0100

    devise hinzugefuegt und routen geaendert

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsTestproject::Application.routes.draw do
+  devise_for :users
+
   get "example/test"
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ RailsTestproject::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'example#test'
 
   # See how all your routes lay out with "rake routes"
 
